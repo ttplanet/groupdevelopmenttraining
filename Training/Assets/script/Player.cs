08f6ea2ff72444afaa6f57110604645911fc3428
ttplanet <twintailsbeadds32@gmail.com>
2018-03-03 20:53:09 +0900

    Training second commit

---
+++ Training/Assets/script/Player.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//プレイヤーの実際の動き
+public class Player {
+
+
+}
---
+++ Training/Assets/script/ProcessFlow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//ゲームの流れ
+public class ProcessFlow : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
