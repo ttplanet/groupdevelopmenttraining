cc430076438b6ae7aee2c67baff2ae04694e2b98
ttplanet <twintailsbeadds32@gmail.com>
2018-03-07 15:06:28 +0900

    仕様変更

--- Training/Assets/script/ProcessFlow.cs
+++ Training/Assets/script/ProcessFlow.cs
@@ -1,13 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//ゲームの流れ
+using UnityEngine.Events;
+//じゃんけんゲームの流れを作るクラス
 public class ProcessFlow : MonoBehaviour {
 
+    private UnityEvent event1;
+    [SerializeField]
+    private SCA sca;
+    
 
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //mainメソッド
+    void Start()
+    {
+        //イベントのインスタンス生成
+        event1 = new UnityEvent();
+        event1.AddListener(Event1);
+    }
+    
+    void Update ()
+    {
+        //タップされた時の動作
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            event1.Invoke();
+
+        }
+    }
+    void Event1()
+    {
+        Debug.Log("イベント発生");
+    }
+
 }
